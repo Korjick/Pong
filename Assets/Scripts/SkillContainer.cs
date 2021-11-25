220df2f4f9a1251a32bab4bac6010f76cbf0b07c
Korjick <zzgriim@gmail.com>
2021-11-25 16:37:56 +0300

    skill container

--- Assets/Scripts/SkillContainer.cs
+++ Assets/Scripts/SkillContainer.cs
@@ -4,15 +4,6 @@ using UnityEngine;
 
 public class SkillContainer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private GameObject _key;
+    [SerializeField] private Transform _progress;
 }
